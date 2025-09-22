cce78467288919a85d0b303075991f01cde2d47e
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-09-22 18:51:13 +0530

    feat: integrate Slot component in RootLayout and update React dependencies

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Slot } from 'expo-router';
 
 const RootLayout = () => {
   return (
     <View>
+      <Slot />
       <Text>RootLayout</Text>
     </View>
   )
