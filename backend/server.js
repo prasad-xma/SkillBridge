1380ca2f99e73c1fc68246c3076245a37f879f7e
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-09-22 16:27:04 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 
 
 
-
+// -------------------- server setup -------------------- //
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
