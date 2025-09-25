41ef05eb701987bd152c6f2799b004e038314f82
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-09-25 13:58:06 +0530

    Integrate expo-splash-screen and update splash logic
    Added expo-splash-screen to dependencies and implemented manual splash screen control in the app layout and splash screen component. The splash screen now hides after the splash animation and navigation, improving the app's startup experience. Also removed 'expo-router/babel' from Babel config and updated the splash screen component export name.

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -1,15 +1,26 @@
 import { StyleSheet, View, useColorScheme } from 'react-native'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Stack } from 'expo-router'
 import { themes } from '../constants/colors'
+import * as SplashScreen from 'expo-splash-screen' 
+
+SplashScreen.preventAutoHideAsync().catch(() => {
+  
+})
 
 const RootLayout = () => {
   const colorScheme = useColorScheme()
   const theme = colorScheme === 'dark' ? themes.dark : themes.light
 
+  const onLayoutRootView = useCallback(async () => {
+    try {
+      await SplashScreen.hideAsync()
+    } catch (e) {}
+  }, [])
+
   return (
-    <View style={{ flex: 1, backgroundColor: theme.background }}>
-      <Stack>
+    <View style={{ flex: 1, backgroundColor: theme.background }} onLayout={onLayoutRootView}>
+      <Stack initialRouteName="splash">
         <Stack.Screen 
           name="splash" 
           options={{ 
