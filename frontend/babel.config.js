61a2a8de2ed1951d1a6127c07e2011883512fa03
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-09-25 11:45:36 +0530

    Add react-native-dotenv for environment variable support
    Introduced react-native-dotenv to manage environment variables in the frontend. Updated login and register components to use API_BASE from .env files, and added related entries to .gitignore. Added Babel config to support the new plugin.

---
+++ frontend/babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'expo-router/babel',
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          allowlist: ['API_BASE'],
+          safe: false,
+          verbose: false,
+        },
+      ],
+    ],
+  };
+};
+
